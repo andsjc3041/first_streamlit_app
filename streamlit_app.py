10e52f3c06cc8e09481ebdcf8f95a4bb185fde62
andsjc3041 <107433689+andsjc3041@users.noreply.github.com>
2022-06-13 14:48:57 -0500

    Update streamlit_app.py
    Added avocado

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🧁 Omega 3 & Blueberry')
 streamlit.text('🥬 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
-streamlit.text('🍞 Avacado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🥝🍍Build Your Own Smoothie 🍌🍇')
